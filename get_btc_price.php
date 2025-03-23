4bcbde163fb99f8b97f67ddfd99bb1a9e6ffbbeb
toishidiary <tkrujka@gmail.com>
2025-03-23 10:48:50 +0200

    Update get_btc_price.php

--- get_btc_price.php
+++ get_btc_price.php
@@ -1,6 +1,6 @@
 <?php
 function getBtcPriceRub() {
-    $apiUrl = "https://api.binance.com/api/v3/ticker/price?symbol=BTCRUB";
+    $apiUrl = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=rub";
     $context = stream_context_create([
         'http' => [
             'timeout' => 10 // Set a timeout for the request
@@ -9,15 +9,15 @@ function getBtcPriceRub() {
     $response = file_get_contents($apiUrl, false, $context);
 
     if (!$response) {
-        error_log("Failed to fetch BTC price from Binance: " . print_r(error_get_last(), true));
+        error_log("Failed to fetch BTC price from CoinGecko: " . print_r(error_get_last(), true));
         return false;
     }
 
     $data = json_decode($response, true);
-    if (!isset($data['price'])) {
-        error_log("Invalid response from Binance API: " . $response);
+    if (!isset($data['bitcoin']['rub'])) {
+        error_log("Invalid response from CoinGecko API: " . $response);
         return false;
     }
 
-    return floatval($data['price']);
+    return floatval($data['bitcoin']['rub']);
 }
