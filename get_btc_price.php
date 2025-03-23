fd1639e0828f1450a8202c58bced67637b2a3440
toishidiary <tkrujka@gmail.com>
2025-03-23 10:41:29 +0200

    Update get_btc_price.php

--- get_btc_price.php
+++ get_btc_price.php
@@ -1,12 +1,23 @@
 <?php
 function getBtcPriceRub() {
     $apiUrl = "https://api.binance.com/api/v3/ticker/price?symbol=BTCRUB";
-    $response = file_get_contents($apiUrl);
+    $context = stream_context_create([
+        'http' => [
+            'timeout' => 10 // Set a timeout for the request
+        ]
+    ]);
+    $response = file_get_contents($apiUrl, false, $context);
 
     if (!$response) {
+        error_log("Failed to fetch BTC price from Binance: " . print_r(error_get_last(), true));
         return false;
     }
 
     $data = json_decode($response, true);
-    return floatval($data['price'] ?? 0);
+    if (!isset($data['price'])) {
+        error_log("Invalid response from Binance API: " . $response);
+        return false;
+    }
+
+    return floatval($data['price']);
 }
