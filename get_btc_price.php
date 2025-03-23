6772ac16e504e92b551401134adec003c9cd7e15
toishidiary <tkrujka@gmail.com>
2025-03-23 10:36:47 +0200

    Update get_btc_price.php

--- get_btc_price.php
+++ get_btc_price.php
@@ -1,23 +1,12 @@
 <?php
-header('Content-Type: application/json');
+function getBtcPriceRub() {
+    $apiUrl = "https://api.binance.com/api/v3/ticker/price?symbol=BTCRUB";
+    $response = file_get_contents($apiUrl);
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-$response = curl_exec($ch);
+    if (!$response) {
+        return false;
+    }
 
-if (curl_errno($ch)) {
-    echo json_encode(['error' => 'Ошибка подключения к Binance']);
-    curl_close($ch);
-    exit;
-}
-
-curl_close($ch);
-$data = json_decode($response, true);
-
-if (isset($data['price'])) {
-    echo json_encode(['btc_price' => floatval($data['price'])]);
-} else {
-    echo json_encode(['error' => 'Ошибка при получении курса BTC']);
+    $data = json_decode($response, true);
+    return floatval($data['price'] ?? 0);
 }
